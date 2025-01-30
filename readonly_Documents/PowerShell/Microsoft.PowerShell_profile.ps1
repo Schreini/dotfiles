15297d1e357043b6c75a860b1b8820b5f182969a
Thomas Schreiner <thomas.schreiner@fleischpruefring.de>
2025-01-30 13:25:46 +0100

    update powershell profile

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,11 @@
-write-output "jaja"
+# Tab Completion for choco; See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Starship Prompt
+Invoke-Expression (&starship init powershell)
+
+# Folder Icons
+Import-Module -Name Terminal-Icons
