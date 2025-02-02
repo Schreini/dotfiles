06c22049bc9f67cfa9fdf0264b28470898e28e97
Thomas Schreiner <t.schreiner@gmail.com>
2025-02-02 14:25:06 +0100

    feat: have scripts to install Powershell Modules

--- .chezmoitemplates/windows/Microsoft.PowerShell_profile.ps1
+++ .chezmoitemplates/windows/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,17 @@
+# Print PowerShell version
+<#
+Write-Output "PowerShell Version: $($PSVersionTable.PSVersion)"
+
+# Check if it's Windows PowerShell or PowerShell Core
+if ($PSVersionTable.PSEdition -eq 'Core') {
+  Write-Output "You are running PowerShell Core (or 7+)."
+} elseif ($PSVersionTable.PSEdition -eq 'Desktop') {
+  Write-Output "You are running Windows PowerShell."
+} else {
+  Write-Output "Unknown PowerShell edition."
+}
+#>
+
 # Tab Completion for choco; See https://ch0.co/tab-completion for details.
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
@@ -8,7 +22,8 @@ if (Test-Path($ChocolateyProfile)) {
 Invoke-Expression (&starship init powershell)
 
 # Folder Icons
-Import-Module -Name Terminal-Icons
+if (Get-Module -Name Terminal-Icons -ListAvailable) { Import-Module -Name Terminal-Icons }
+else {Write-Output "No Terminal-Icons"}
 
 # Aliases
 New-Alias -Name chez -Value chezmoi
