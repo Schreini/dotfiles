d54bfa28c95bf7dd74bb2e51bb2ac208ee8b9eb8
Thomas Schreiner <t.schreiner@gmail.com>
2025-05-15 21:52:52 +0200

    just some work

--- .chezmoiscripts/windows/run_onchange_00-install-essentials.ps1
+++ .chezmoiscripts/windows/run_onchange_00-install-essentials.ps1
@@ -3,4 +3,7 @@ write-output "# Installing Essentials"
 write-output "#"
 winget install --silent --accept-source-agreements --accept-package-agreements --disable-interactivity --exact Microsoft.PowerShell
 winget install --silent --accept-source-agreements --accept-package-agreements --disable-interactivity --exact gerardog.gsudo
+
+$env:path=[System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+
 gsudo config CacheMode Auto
