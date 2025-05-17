271d2dff79a17ca630c29b08292487a687c43883
Thomas Schreiner <t.schreiner@gmail.com>
2025-05-17 16:57:58 +0200

    add language installation to windows setup script

--- .chezmoiscripts/windows/run_onchange_06-configure-windows.ps1
+++ .chezmoiscripts/windows/run_onchange_06-configure-windows.ps1
@@ -1,5 +1,15 @@
 write-output "#"
 write-output "# Configuring Windows"
 write-output "#"
+
+write-output "# uninstall onedrive"
 gsudo winget uninstall --silent --accept-source-agreements --disable-interactivity --exact Microsoft.OneDrive
 gsudo winget uninstall --silent --accept-source-agreements --disable-interactivity onedrive
+
+write-output "# install english language"
+gsudo install-language en-US
+
+write-output "# enable english keyboard"
+$langList = Get-WinUserLanguageList
+$langList.Add("en-US")
+Set-WinUserLanguageList -LanguageList $langList
