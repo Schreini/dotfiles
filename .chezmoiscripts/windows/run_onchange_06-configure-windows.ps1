cb8504befbc11c90e5496a200477b3aeb5c94952
Thomas Schreiner <t.schreiner@gmail.com>
2025-06-08 12:04:34 +0200

    feat: disable windows suggestion notifications

--- .chezmoiscripts/windows/run_onchange_06-configure-windows.ps1
+++ .chezmoiscripts/windows/run_onchange_06-configure-windows.ps1
@@ -18,3 +18,14 @@ Set-WinUserLanguageList -LanguageList $langList -Force
 
 # previos operation automagically sets Display language to english, so force german as display language
 Set-WinUILanguageOverride -Language de-DE
+
+# Disable Windows Suggestion Advertisments Notifications (Vorschläge Benachrichtigungen)
+# https://de.windows-office.net/?p=9279
+$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
+Set-ItemProperty -Path $regPath -Name "SubscribedContent-338389Enabled" -Value 0 -Force
+Set-ItemProperty -Path $regPath -Name "SubscribedContent-310093Enabled" -Value 0 -Force
+
+# https://www.elevenforum.com/t/enable-or-disable-suggested-content-in-settings-in-windows-11.3791/
+Set-ItemProperty -Path $regPath -Name "SubscribedContent-338393Enabled" -Value 0 -Force
+Set-ItemProperty -Path $regPath -Name "SubscribedContent-353694Enabled" -Value 0 -Force
+Set-ItemProperty -Path $regPath -Name "SubscribedContent-353696Enabled" -Value 0 -Force
